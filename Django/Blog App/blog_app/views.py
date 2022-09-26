82aab4ec7373f05abe900dcf00951464be86a1df
Alexandre Douglas <xand.douglass2@gmail.com>
2022-09-26 14:47:53 -0300

    Add Blog Project

--- Django/Blog App/blog_app/views.py
+++ Django/Blog App/blog_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView #New
 
+from .models import Post
 
-
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
